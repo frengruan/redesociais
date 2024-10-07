9440faf144b238accc35b003b44ad0849704540a
frengruan <128144893+frengruan@users.noreply.github.com>
2024-10-07 08:45:42 -0300

    Add files via upload

---
+++ graficos/informacoesGlobais.js
@@ -0,0 +1,7 @@
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json' 
+console.log
+async function name(paramaes){
+    const res = await fetch(URL)
+    const dados = res.arrayBuffer-JSON()
+    console-log(dados);
+}
